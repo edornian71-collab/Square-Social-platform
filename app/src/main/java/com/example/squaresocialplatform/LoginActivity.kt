51db3d8034d282e0c888c4e1c1a9ba8b5c6c19fe
Ethan Dornian <edornian71@gmail.com>
2026-05-12 17:55:53 -0600

    Setting up logic for login page

--- app/src/main/java/com/example/squaresocialplatform/LoginActivity.kt
+++ app/src/main/java/com/example/squaresocialplatform/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.squaresocialplatform
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,6 +14,22 @@ class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.loginlayout)
+
+        // input fields and login buttton
+        val emailInput = findViewById<EditText>(R.id.email)
+        val passInput = findViewById<EditText>(R.id.password)
+
+        val loginBtn = findViewById<Button>(R.id.loginBtn)
+
+        loginBtn.setOnClickListener {
+
+            val email = emailInput.text.toString().trim()
+            val password = passInput.text.toString().trim()
+
+
+        }
+
+
 
 
 
