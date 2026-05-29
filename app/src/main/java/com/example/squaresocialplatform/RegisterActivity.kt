d36983704ab52128efd5a1c73a7751f6764a0182
Ethan Dornian <edornian71@gmail.com>
2026-05-28 23:05:46 -0600

    Start of the logic for login and register activity.

--- app/src/main/java/com/example/squaresocialplatform/RegisterActivity.kt
+++ app/src/main/java/com/example/squaresocialplatform/RegisterActivity.kt
@@ -7,22 +7,24 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-class LoginActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
+    private lateinit var etUserName: EditText
     private lateinit var etEmail: EditText
     private lateinit var etPassword: EditText
-    private lateinit var btnLogin: Button
+    private lateinit var btnRegister: Button
     private lateinit var db: DatabaseHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
 
         db = DatabaseHelper(this)
 
-        etEmail = findViewById(R.id.email)
-        etPassword = findViewById(R.id.password)
-        btnLogin = findViewById(R.id.loginBtn)
+        etUserName = findViewById(R.id.userName)
+        etEmail = findViewById(R.id.newEmail)
+        etPassword = findViewById(R.id.newPass)
+        btnRegister = findViewById(R.id.regBtn)
 
     }
 }
